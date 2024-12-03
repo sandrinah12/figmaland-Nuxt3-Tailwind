877eb4995310e25924fe3fe2bb6c06fafb2490dd
SandrinahMiah <sandrinahrasoamihanta@gmail.com>
2024-12-03 17:31:38 +0200

    Ajout de commentaires détaillés

--- plugins/google-maps.js
+++ plugins/google-maps.js
@@ -3,7 +3,7 @@ import VueGoogleMaps from "@fawmi/vue-google-maps";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGoogleMaps, {
     load: {
-      key: "AIzaSyAtYWHVLHKVgK0jg8Qe89Tei8rvpuQ7dCk", // Remplacez par votre clé API
+      key: "", // Remplacez par votre clé API
     },
   });
 });
